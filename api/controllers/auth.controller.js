ca2a4d75efb74ad4456614cfddf3aa4be0614bb0
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-02-26 12:36:43 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,19 +1,39 @@
-import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async(req, res, next) =>{
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password : hashedPassword });
-  try{
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
     await newUser.save();
-    res.status(201).json(
-      'User created successfully'
-    );
-  }catch(error){  
+    res.status(201).json("User created successfully");
+  } catch (error) {
     // res.status(500).json(error.message);
     next(error);
     // next(errorHandler(550, 'error from the function')); this is user defined error
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validuser = await User.findOne({ email });
+    if (!validuser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validuser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid password"));
+
+    const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest} = validuser._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
