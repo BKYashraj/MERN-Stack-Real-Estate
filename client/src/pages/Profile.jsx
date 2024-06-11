d7638c4bb6b348f00930bb3ed0b5c4e6fec60fd8
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-06-12 02:20:10 +0530

    Profile Section Updated

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,32 @@
 
+import {useSelector} from 'react-redux'
 
 export default function Profile() {
+  const {currentUser} =  useSelector(state => state.user)
   return (
-    <div>
-      <h1>Profile section updated</h1>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7 ">Profile</h1>
+
+      <form className="flex flex-col gap-4">
+        <img src={currentUser.avatar}  alt="profile" className="rounded-full h-24 w-24 object-cover mx-auto cursor-pointer self-center mt-2"
+        />
+
+        <input id="username" type="text" placeholder="username" className="border p-3 rounded-lg" />
+
+        <input id="email" type="text" placeholder="email" className="border p-3 rounded-lg" />
+
+        <input id="password" type="text" placeholder="password" className="border p-3 rounded-lg" />
+
+        <button className="bg-slate-700 text-white p-3  rounded-lg uppercase hover:opacity-90 disabled:opacity-80">Update</button> 
+
+      </form>
+
+      <div className="flex justify-between mt-5">
+          <span className="text-red-700 cursor-pointer">Delete account</span>
+
+          <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+
     </div>
   )
 }
